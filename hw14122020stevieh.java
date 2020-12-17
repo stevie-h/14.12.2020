15de15c53299c3be5b7adbfa94c2149c816f687f
Stevie H <74920914+stevie-h@users.noreply.github.com>
2020-12-17 16:36:55 +0200

    Update hw14122020stevieh.java

--- hw14122020stevieh.java
+++ hw14122020stevieh.java
@@ -1,1 +1,27 @@
+/* HW 14.12.2020
+1. write all numbers from -1000 to +1000 using do-while loop
+2. read oreh,rohav,hekef from user until the hekef was 2*oreh+2*rohav using do-while loop
+   for exmaple:
+   oreh=3, rohav=4, hekef=12 is wrong, since 3*2 + 4*2 = 14... so the user should input the numbers again
+   oreh=1, rohav=5, hekef=12 is correct since 1*2 + 5*2 = 12... so the loop should end after this input
+3. now solve targil 1,2 to using while(true)+break loop 
+4. read temperature from user (float)
+   if temperature < 35 print "out-of-range" + break 
+   if temperature > 42 print "out-of-range" + break
+   if temperature > 37.5 and temperature <= 42 print "sick"
+   if temperature > 35 and temperature <= 37.5 print "healthy"
+   use while (true) + break
+*/
 
+package com.company;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+      
+      
+      
+      
